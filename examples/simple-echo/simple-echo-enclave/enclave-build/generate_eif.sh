12cd248b52a1822fe951019d08f0c2beab6cd2ff
Ubuntu <sunchaoqun@126.com>
2023-05-30 22:03:39 +0800

    change docker uri

--- examples/simple-echo/simple-echo-enclave/enclave-build/generate_eif.sh
+++ examples/simple-echo/simple-echo-enclave/enclave-build/generate_eif.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-nitro-cli build-enclave --docker-uri simple-echo-enclave:latest --output-file bin/simple-echo-enclave.eif
+nitro-cli build-enclave --docker-uri public.ecr.aws/s0k4s5w7/enclave-wallet --output-file bin/enclave-wallet.eif
