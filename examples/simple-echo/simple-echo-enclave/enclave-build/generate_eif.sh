69204fb6f61eb6b3cf7cbf1c76fa7f23ff990539
Ubuntu <sunchaoqun@126.com>
2023-05-30 21:58:05 +0800

    add enclave build process

---
+++ examples/simple-echo/simple-echo-enclave/enclave-build/generate_eif.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -e
+
+nitro-cli build-enclave --docker-uri simple-echo-enclave:latest --output-file bin/simple-echo-enclave.eif
---
+++ examples/simple-echo/simple-echo-enclave/enclave-build/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+# Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+readonly EIF_PATH="/home/hello.eif"
+readonly ENCLAVE_CPU_COUNT=2
+readonly ENCLAVE_MEMORY_SIZE=3396
+
+main() {
+    nitro-cli run-enclave --cpu-count $ENCLAVE_CPU_COUNT --memory $ENCLAVE_MEMORY_SIZE \
+        --eif-path $EIF_PATH --debug-mode
+
+    local enclave_id=$(nitro-cli describe-enclaves | jq -r ".[0].EnclaveID")
+    echo "-------------------------------"
+    echo "Enclave ID is $enclave_id"
+    echo "-------------------------------"
+
+    nitro-cli console --enclave-id $enclave_id # blocking call.
+}
+
+main
